93f4de3b1e0763733ed9b9d1e9c2f178a2c61009
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-12-16 22:26:07 +0330

    created first view

--- Django_Introduction/polls/views.py
+++ Django_Introduction/polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    """processing - database , cache , rendering Html , """
+    print(type(request))
+    print(request.user)
+    # if request.method == "POST":
+    return HttpResponse("Hello, world. You're at the polls index 222.")
+
+
