457a6bd809bf8009aef2dc7bb6f0831bcf463d86
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-12-29 19:18:15 +0330

    work on view and render tempalte on server

--- Django_Introduction/polls/views.py
+++ Django_Introduction/polls/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Question
+from django.template import loader
 
 def index(request):
-    """processing - database , cache , rendering Html , """
-    print(type(request))
-    print(request.user)
-    # if request.method == "POST":
-    return HttpResponse("Hello, world. You're at the polls index 222.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
 
 
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
