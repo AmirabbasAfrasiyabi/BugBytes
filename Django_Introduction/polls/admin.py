92d773e8d247a52b20195a76cfe4c7a4a131d03e
AmirabbasAfrasiyabi <amirabbasafrasiyabi1998@gmail.com>
2026-07-31 19:32:25 +0330

    updated

--- Django_Introduction/polls/admin.py
+++ Django_Introduction/polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Question,Choice
 
+admin.site.site_header = "poll admin"
+admin.site.site_title = ("the world slickest admin panel")
+admin.site.index_title = "admin UI"
+
 admin.site.register(Question)
 
 admin.site.register(Choice)
